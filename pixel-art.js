02c5deca01da72dd831ed1d363a1c96e6b45a204
Linda Pei <linda.pei@gmail.com>
2017-08-06 18:10:29 -0700

    got the brush to work! also added custom color

--- pixel-art.js
+++ pixel-art.js
@@ -1,4 +1,4 @@
-
+//Creates a pixel drawing canvas with a div grid
 for (i = 1; i <= 40; i ++ ) {
     var row = document.createElement("div");
     row.className = 'row';
@@ -15,6 +15,8 @@ for (i = 1; i <= 40; i ++ ) {
 
     }
 }
+
+//Stores the palette color and pushes to UI to display the palette color
 var colorDiv = document.getElementsByClassName("paletteColor");
 for (c = 0; c < colorDiv.length; c ++) {
     colorDiv[c].addEventListener('click', function(event) {
@@ -22,10 +24,37 @@ for (c = 0; c < colorDiv.length; c ++) {
         document.querySelector(".currentColor").style.backgroundColor = currentColor;
     });
 }
+
+//Update current color from a change in the Custom Color input
+function updateCurrentColor () {
+    var currentColor = document.querySelector("input").value;
+    console.log(currentColor);
+    document.querySelector(".currentColor").style.backgroundColor = currentColor;
+}
+
+//Creates click event listener to apply palette color to the pixel
+// var ediv = document.getElementsByClassName("griditem");
+// for (e = 0; e < ediv.length; e ++) {
+//     ediv[e].addEventListener('click', updateColor);
+// }
+
+//Applies color based on mouse behavior (brush like)
 var ediv = document.getElementsByClassName("griditem");
+var isMouseDown = false;
 for (e = 0; e < ediv.length; e ++) {
-    ediv[e].addEventListener('click', function(event) {
-        this.style.border = "0px";
-        this.style.backgroundColor = document.querySelector(".currentColor").style.backgroundColor;
+    ediv[e].addEventListener('mousedown', function(){
+        isMouseDown = true;
+    });
+    ediv[e].addEventListener('mousemove', function () {
+        if (isMouseDown) {
+            currentColor = document.querySelector(".currentColor").style.backgroundColor;
+            this.style.borderColor = currentColor;
+            this.style.backgroundColor = currentColor;
+        }
+    });
+    ediv[e].addEventListener('mouseup', function () {
+        isMouseDown = false;
     });
 }
+
+
